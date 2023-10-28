ce0c6507f6ecda5c85c2dc8caf8a95cd3494b339
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:22:54 +0530

    Update 1464-maximum-product-of-two-elements-in-an-array.cpp

--- Leetcode Solutions/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ Leetcode Solutions/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
+    // This function, 'maxProduct', takes a vector of integers 'nums' as input and
+    // returns the maximum product of two distinct elements from the input vector.
+
     int maxProduct(vector<int>& nums) {
-        int max =0, t;
-        for(int i=0; i<nums.size()-1; i++){
-            for(int j = i+1; j<nums.size(); j++){
-                t = (nums[i]-1)*(nums[j]-1);
-                    max = t>max?t:max;
+        // Initialize 'max' to store the maximum product and 't' as a temporary variable.
+        int max = 0, t;
+
+        // Use two nested loops to compare all possible pairs of distinct elements from the input vector.
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                // Calculate the product of two distinct elements and store it in 't'.
+                t = (nums[i] - 1) * (nums[j] - 1);
+
+                // Update 'max' with the maximum value between 't' and the current 'max'.
+                max = t > max ? t : max;
             }
         }
+
+        // Return the maximum product found.
         return max;
     }
-};
+};  // End of the Solution class.
