02927f704e6571360a4b0b13e9ce68d41054d7c9
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:26:59 +0530

    Update 222-count-complete-tree-nodes.cpp

--- Leetcode Solutions/222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ Leetcode Solutions/222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -9,20 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+    // This function, 'countNodes', counts the number of nodes in a binary tree given its root node.
+
     int countNodes(TreeNode* root) {
-        if(root == NULL) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int c=0;
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            c++;
-            if(temp->right) q.push(temp->right);
-            if(temp->left) q.push(temp->left);
+        if (root == NULL) {
+            return 0;  // If the tree is empty (root is NULL), return 0.
         }
-        return c;
+
+        queue<TreeNode*> q;  // Create a queue to perform level-order traversal.
+        q.push(root);  // Enqueue the root node to start the traversal.
+
+        int count = 0;  // Initialize a count to keep track of the number of nodes.
+
+        // Perform a level-order traversal of the binary tree.
+        while (!q.empty()) {
+            TreeNode* temp = q.front();
+            q.pop();  // Dequeue the current node.
+
+            count++;  // Increment the count for the current node.
+
+            // Enqueue the left and right child nodes if they exist.
+            if (temp->left) {
+                q.push(temp->left);
+            }
+            if (temp->right) {
+                q.push(temp->right);
+            }
+        }
+
+        return count;  // Return the total number of nodes in the binary tree.
     }
 };
