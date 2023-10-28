813ff70cbafce136c3295856e4d8e6570c3a2d7b
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:30:55 +0530

    Update 387-first-unique-character-in-a-string.cpp

--- Leetcode Solutions/387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ Leetcode Solutions/387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    // This function, 'firstUniqChar', finds and returns the index of the first non-repeating character in the input string 's'.
+
     int firstUniqChar(string s) {
-        map<char, int> m;
-        for(char i: s){
+        map<char, int> m;  // Create a map to store the frequency of each character in 's'.
+
+        // Populate the map 'm' with character frequencies from the string 's'.
+        for (char i : s) {
             m[i]++;
         }
-        for(int i=0; i<s.size(); i++){
-            if(m[s[i]] == 1) return i;
+
+        // Iterate through the characters of the string 's' to find the first non-repeating character.
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == 1) {
+                return i;  // If the character occurs only once, return its index as the first non-repeating character.
+            }
         }
+
+        // If no non-repeating character is found, return -1 to indicate that there are no non-repeating characters in the string.
         return -1;
     }
 };
