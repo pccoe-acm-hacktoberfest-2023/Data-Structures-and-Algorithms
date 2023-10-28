430e129054b004e94299dcf4ce51e538874c2c71
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:24:56 +0530

    Update 1935-maximum-number-of-words-you-can-type.cpp

--- Leetcode Solutions/1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
+++ Leetcode Solutions/1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+    // This function, 'canBeTypedWords', counts the number of words in a given 'text' string
+    // that can be typed without using any letters present in the 'brokenLetters' string.
+
     int canBeTypedWords(string text, string brokenLetters) {
-        bool isbroken=false;
-        int ans=0;
-        map<char, int> m;
-        for(char x : brokenLetters) m[x]++;
-        for(int i=0; i<text.size(); i++){
-            if(m[text[i]]>0){
-                isbroken=true;
+        bool isbroken = false; // Flag to track if a word contains broken letters.
+        int ans = 0; // Initialize the count of words that can be typed.
+        map<char, int> m; // Create a map to store the count of broken letters.
+
+        // Populate the 'm' map with the count of each character from 'brokenLetters'.
+        for (char x : brokenLetters) {
+            m[x]++;
+        }
+
+        // Iterate through the characters in the 'text' string.
+        for (int i = 0; i < text.size(); i++) {
+            if (m[text[i]] > 0) {
+                isbroken = true; // Set the flag to true if the current character is in 'brokenLetters'.
             }
-            if(text[i] == ' ' || i== text.size()-1){
-                if(!isbroken){
-                    ans++;
+
+            // Check if the current character is a space (end of a word) or the last character in the 'text' string.
+            if (text[i] == ' ' || i == text.size() - 1) {
+                if (!isbroken) {
+                    ans++; // If the word doesn't contain broken letters, increment the count.
                 }
-                isbroken=false;
+                isbroken = false; // Reset the flag for the next word.
             }
         }
+
+        // Return the count of words that can be typed without broken letters.
         return ans;
     }
 };
