ae3a860da1c758ccc15c645637cf375644c2af20
surajTade <suraj.tade20@pccoepune.org>
2023-10-28 13:02:01 +0530

    minor changes

--- Sorting Algorithms/Bubble_Sort.c
+++ Sorting Algorithms/Bubble_Sort.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-//Bubble Sort Function
-void bubbleSort(int arr[], int n) {
+// Bubble Sort Function
+void mergeSort(int arr[], int n)
+{
     int swapped;
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++)
+    {
         swapped = 0; // No swaps at the beginning
 
         // Let's check each pair of adjacent elements
-        for (int j = 0; j < n - i - 1; j++) {
+        for (int j = 0; j < n - i - 1; j++)
+        {
             // If the current element is bigger than the next one, swap them
-            if (arr[j] > arr[j + 1]) {
+            if (arr[j] > arr[j + 1])
+            {
                 // Swap them!
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -19,26 +23,30 @@ void bubbleSort(int arr[], int n) {
         }
 
         // If we didn't swap anything in this pass, it means the array is sorted
-        if (swapped == 0) {
+        if (swapped == 0)
+        {
             break;
         }
     }
 }
 
-int main() {
+int main()
+{
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     printf("Original array: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
 
     // Call the Bubble Sort function
-    bubbleSort(arr, n);
+    mergeSort(arr, n);
 
     printf("\nSorted array: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
