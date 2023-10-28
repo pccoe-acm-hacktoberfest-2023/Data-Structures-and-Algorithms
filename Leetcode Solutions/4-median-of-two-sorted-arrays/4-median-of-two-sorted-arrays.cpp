1d59fc8b443e89c1fe02cd3f5ca49a251f7a218e
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:31:13 +0530

    Update 4-median-of-two-sorted-arrays.cpp

--- Leetcode Solutions/4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ Leetcode Solutions/4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
+    // This function, 'findMedianSortedArrays', finds the median of two sorted arrays, 'nums1' and 'nums2'.
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        for(auto i : nums2) nums1.push_back(i);
-        sort(nums1.begin(), nums1.end());
-        if(nums1.size()%2==0){
-            return 1.0*(nums1[nums1.size()/2-1]+nums1[nums1.size()/2])/2;
+        // Merge 'nums2' into 'nums1'.
+        for (auto i : nums2) {
+            nums1.push_back(i);
         }
-        return nums1[nums1.size()/2];
-    }
-};
+
+        // Sort the merged array.
+        sort(nums1.begin(), nums1.end());
+
+        // Check if the merged array has an even or odd number of elements.
+        if (nums1.size() % 2 == 0) {
+            // If the number of elements is even, calculate and return the average of the two middle elements.
+            return 1.0 * (nums1[nums1.size() / 2 - 1] + nums1[nums1.size() / 2]) / 2;
+        } else {
+            // If the number
