0eef9d65cfcf9ca2fc49e0c82191f507b5238d9b
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:18:13 +0530

    Update 11-container-with-most-water.cpp

--- Leetcode Solutions/11-container-with-most-water/11-container-with-most-water.cpp
+++ Leetcode Solutions/11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
+    // This function, 'maxArea', takes a vector of integers 'h' as input, where 'h'
+    // represents the heights of vertical lines. It calculates and returns the maximum
+    // area between two lines that can be formed by selecting any two vertical lines.
+
     int maxArea(vector<int>& h) {
-        int ans=0,l=0, r=h.size()-1, area;
-        while(l<r){
-            if(h[l] < h[r]){
-                area = h[l]*(r-l);
+        // Initialize variables: 'ans' to store the maximum area, 'l' for the left pointer,
+        // 'r' for the right pointer, and 'area' for the current area being calculated.
+        int ans = 0, l = 0, r = h.size() - 1, area;
+        
+        // Use a while loop to traverse the array and calculate the maximum area.
+        while (l < r) {
+            if (h[l] < h[r]) {
+                // Calculate the area using the height of the left line ('h[l]')
+                // and the width between the lines ('r - l'). Then, move the left pointer 'l'.
+                area = h[l] * (r - l);
                 l++;
             }
-            else{
-                area = h[r]*(r-l);
+            else {
+                // Calculate the area using the height of the right line ('h[r]')
+                // and the width between the lines ('r - l'). Then, move the right pointer 'r'.
+                area = h[r] * (r - l);
                 r--;
             }
-            ans = ans>area?ans:area;
+            
+            // Update 'ans' with the maximum value between 'ans' and the current 'area'.
+            ans = ans > area ? ans : area;
         }
+        
+        // Return the maximum area found.
         return ans;
     }
-};
+};  // End of the Solution class.
