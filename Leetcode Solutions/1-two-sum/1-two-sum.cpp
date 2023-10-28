bd4a4f9c68efb0c8f174d7ae92f421cdde2acbcb
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:17:51 +0530

    Update 1-two-sum.cpp

--- Leetcode Solutions/1-two-sum/1-two-sum.cpp
+++ Leetcode Solutions/1-two-sum/1-two-sum.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
+    // This function takes a vector of integers (nums) and an integer (target) as input.
+    // It finds and returns a vector containing the indices of two numbers from nums
+    // that add up to the given target.
+
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Create a map to store the elements of nums and their corresponding indices.
         map<int, int> m;
+        
+        // Iterate through the elements of nums using a for loop.
         for(int i=0; i<nums.size(); i++){
-            if(m.find(nums[i])!=m.end()){
+            // Check if the current element nums[i] is present in the map.
+            if(m.find(nums[i]) != m.end()){
+                // If the current element is found in the map, return a vector
+                // containing the indices of the two elements that sum up to the target.
                 return vector<int>{m[nums[i]], i};
             }
-            m[target-nums[i]] = i;
+            
+            // Calculate the difference between the target and the current element.
+            // Store this difference as the key in the map with the value being the current index.
+            m[target - nums[i]] = i;
         }
+        
+        // If no such pair of elements is found, return the input nums (which may not be
+        // very meaningful, but it keeps the return type consistent).
         return nums;
-        // int sum=0,a,b;
-        // for(int i=0;i<nums.size();i++){
-        //     for(int j=0;j<nums.size();j++){
-        //         if(i!=j){
-        //         sum=nums[i]+nums[j];
-        //         if(sum==target){
-        //              a=i;
-        //              b=j;
-        //         }
-        //         }
-        //     }
-        // }
-        // return {a,b};
     }
-    
-};
+};  // End of the Solution class.
