661fb2a27caf2f3f40e0a2912c8730d8e733d8db
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:37:25 +0530

    Update 706-design-hashmap.cpp

--- Leetcode Solutions/706-design-hashmap/706-design-hashmap.cpp
+++ Leetcode Solutions/706-design-hashmap/706-design-hashmap.cpp
@@ -1,30 +1,23 @@
 class MyHashMap {
 public:
-    vector<int> m;
-    int size;
+    vector<int> m; // Vector to store values
+    int size;     // Size of the HashMap
+
     MyHashMap() {
-        size = 1e6+1;
-        m.resize(size);
-        fill(m.begin(), m.end(),-1);
+        size = 1e6 + 1;   // Initialize the size to a large value
+        m.resize(size);   // Resize the vector to accommodate the specified size
+        fill(m.begin(), m.end(), -1);  // Fill the vector with -1 to indicate empty slots
     }
-    
+
     void put(int key, int value) {
-        m[key]=value;
+        m[key] = value;  // Store the value in the vector at the corresponding key
     }
-    
+
     int get(int key) {
-        return m[key];
+        return m[key];  // Retrieve the value from the vector using the key
     }
-    
+
     void remove(int key) {
-        m[key]= -1;
+        m[key] = -1;  // Remove the value by setting it to -1 in the vector
     }
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
