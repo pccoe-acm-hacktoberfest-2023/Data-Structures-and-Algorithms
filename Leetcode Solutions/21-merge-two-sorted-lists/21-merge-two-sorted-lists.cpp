58f5d7a4a25a1031000af247f49db3c2c0ee0e34
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:26:19 +0530

    Update 21-merge-two-sorted-lists.cpp

--- Leetcode Solutions/21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ Leetcode Solutions/21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,20 +8,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    ListNode* merge(ListNode* l1, ListNode* l2){
-        if(l1==NULL) return l2;
-        if(l2==NULL) return l1;
-        if(l1->val<l2->val){
-            l1->next = merge(l1->next, l2);
+    // This function, 'merge', merges two sorted linked lists 'l1' and 'l2' and returns the merged list.
+
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        // Base cases: If either of the lists is empty, return the other list.
+        if (l1 == NULL) {
+            return l2;
+        }
+        if (l2 == NULL) {
             return l1;
         }
-        else{
+
+        // Compare the values of the current nodes in 'l1' and 'l2'.
+        if (l1->val < l2->val) {
+            // If the value in 'l1' is smaller, merge 'l1->next' with 'l2' and return 'l1'.
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            // If the value in 'l2' is smaller or equal, merge 'l1' with 'l2->next' and return 'l2'.
             l2->next = merge(l1, l2->next);
             return l2;
         }
     }
+
+    // This function, 'mergeTwoLists', is a wrapper for the 'merge' function and merges two sorted linked lists.
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         return merge(list1, list2);
     }
