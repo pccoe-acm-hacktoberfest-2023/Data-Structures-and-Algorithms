a60cc8bc54c3efd2bb6b0653c054a20d2ec184e5
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:28:55 +0530

    Update 242-valid-anagram.cpp

--- Leetcode Solutions/242-valid-anagram/242-valid-anagram.cpp
+++ Leetcode Solutions/242-valid-anagram/242-valid-anagram.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
+    // This function, 'isAnagram', checks if two input strings, 's' and 't', are anagrams of each other.
+
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        map<char, int> m1, m2;
-        for(char i: s){
+        // Check if the lengths of the two strings are different; if so, they cannot be anagrams.
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        map<char, int> m1, m2;  // Create two maps to store character frequencies in 's' and 't'.
+
+        // Populate 'm1' with the character frequencies in string 's'.
+        for (char i : s) {
             m1[i]++;
         }
-        for(char i: t){
+
+        // Populate 'm2' with the character frequencies in string 't'.
+        for (char i : t) {
             m2[i]++;
         }
-        for(char i: s){
-            if(m1[i] != m2[i]) return false;
-        } 
+
+        // Compare the character frequencies between 's' and 't'. If any character frequency is different,
+        // the strings are not anagrams.
+        for (char i : s) {
+            if (m1[i] != m2[i]) {
+                return false;
+            }
+        }
+
+        // If no differences were found, the strings are anagrams.
         return true;
     }
 };
