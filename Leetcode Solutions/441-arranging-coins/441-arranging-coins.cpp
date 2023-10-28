db7ef6538ba11bed799eee4a55de9b00f0416fa3
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:32:34 +0530

    Update 441-arranging-coins.cpp

--- Leetcode Solutions/441-arranging-coins/441-arranging-coins.cpp
+++ Leetcode Solutions/441-arranging-coins/441-arranging-coins.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        // sqrt of n 
-        // long i = 0, sum = 0;
-        // while (sum <= n) sum += ++i;
-        // return i-1;
-        
-        // log n
         long l = 0, h = n, mid, temp;
+
+        // Perform binary search to find the maximum number of complete rows.
         while (l <= h) {
-            mid = l + (h - l) / 2;
-            temp = mid * (mid + 1) / 2;
-            
-            if (temp == n) return mid;
-            
-            if (temp <= n) l = mid + 1;
-            else h = mid - 1;
+            mid = l + (h - l) / 2;   // Calculate the middle point.
+            temp = mid * (mid + 1) / 2;  // Calculate the total number of coins in the first 'mid' rows.
+
+            if (temp == n) {
+                return mid;  // If the total matches 'n', return 'mid' as the answer.
+            }
+
+            if (temp <= n) {
+                l = mid + 1;  // If the total is less than 'n', search the right half.
+            } else {
+                h = mid - 1;  // If the total is greater than 'n', search the left half.
+            }
         }
-        return l-1;
+
+        return l - 1;  // Return 'l - 1' as the number of complete rows because 'l' counts one row too many.
     }
 };
