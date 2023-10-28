20a59c9e430549bec89c9ae5a9b762a0b116462c
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:37:57 +0530

    Update 75-sort-colors.cpp

--- Leetcode Solutions/75-sort-colors/75-sort-colors.cpp
+++ Leetcode Solutions/75-sort-colors/75-sort-colors.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int l=0,mid=0, h=nums.size()-1;
-        while(mid<=h){
-            if(nums[mid]==0){
-                swap(nums[l++],nums[mid++]);
-            }
-            else if(nums[mid]==1) mid++;
-            else{
+        int l = 0;       // Left pointer for 0s
+        int mid = 0;     // Middle pointer for 1s
+        int h = nums.size() - 1; // Right pointer for 2s
+
+        while (mid <= h) {
+            if (nums[mid] == 0) {
+                // If the element at mid is 0, move it to the left side.
+                swap(nums[l++], nums[mid++]);
+            } else if (nums[mid] == 1) {
+                // If the element at mid is 1, it's already in the correct position.
+                mid++;
+            } else {
+                // If the element at mid is 2, move it to the right side.
                 swap(nums[mid], nums[h--]);
             }
         }
