3e1b73eba542a5e8779a9e43602412c61063a76f
chiku <chintukumar0460@gmail.com>
2023-10-28 16:43:49 +0530

    due to conflict problem i created a new folder and added sorting algorithems

---
+++ sorting Algorithms by chiku011/Selection_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Selection Sort Function
+void selectionSort(int arr[], int n)
+{
+    // Write your code here
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    // Call the Selection Sort function
+    selectionSort(arr, n);
+
+    printf("\nSorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+/*
+Output:
+
+Original array: 64 34 25 12 22 11 90
+Sorted array: 11 12 22 25 34 64 90
+*/
