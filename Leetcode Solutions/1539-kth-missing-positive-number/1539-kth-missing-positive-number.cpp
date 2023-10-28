54f81304d243e5ad36258ba88249b4ebcfcbac76
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:23:39 +0530

    Update 1539-kth-missing-positive-number.cpp

--- Leetcode Solutions/1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ Leetcode Solutions/1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    // This function, 'findKthPositive', finds and returns the kth positive integer that is missing
+    // from a given array 'arr' of positive integers. The function uses a simple iterative approach.
+
     int findKthPositive(vector<int>& arr, int k) {
-        // unordered_map<int,int>m;
-        // for(int i: arr) m[i]++;
-        // for(int i=1;; i++){
-        //     if(m[i] == 0) k--;
-        //     if(k==0) return i;
-        // }
-        // return -1;
-        for(int i: arr) if(i<=k) k++;
-        return k; // beacause those many elements were there in the array
+        // Iterate through the elements of the 'arr' and adjust 'k' for each element found in the array.
+        for (int i : arr) {
+            if (i <= k) {
+                k++;  // Increment 'k' as we've found an element that's not missing.
+            }
+        }
+
+        // 'k' will now represent the kth missing positive integer.
+        return k;
     }
 };
