f86a77b998fb564253b4fa87356ecb90ed313ea9
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:33:31 +0530

    Update 48-rotate-image.cpp

--- Leetcode Solutions/48-rotate-image/48-rotate-image.cpp
+++ Leetcode Solutions/48-rotate-image/48-rotate-image.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-         int n=matrix.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {   
-               swap(matrix[i][j],matrix[j][i]);
+        int n = matrix.size();  // Get the size of the square matrix.
+
+        // Transpose the matrix by swapping elements across the main diagonal.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i = 0; i < n; ++i){
+
+        // Reverse each row of the transposed matrix to achieve a 90-degree clockwise rotation.
+        for (int i = 0; i < n; ++i) {
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
