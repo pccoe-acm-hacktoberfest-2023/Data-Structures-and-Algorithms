c3b8e709015dfb78040d2fe148566bc292685a96
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2023-10-29 00:24:37 +0530

    Update 1720-decode-xored-array.cpp

--- Leetcode Solutions/1720-decode-xored-array/1720-decode-xored-array.cpp
+++ Leetcode Solutions/1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
+    // This function, 'decode', takes a vector of integers 'encoded' and an integer 'first' as input.
+    // It decodes the 'encoded' vector and returns the decoded result as a vector of integers.
+
     vector<int> decode(vector<int>& encoded, int first) {
-        encoded.insert(encoded.begin(),first);
-        for(int i=1; i<encoded.size(); i++) encoded[i] = encoded[i-1]^encoded[i];
+        // Insert the 'first' element at the beginning of the 'encoded' vector.
+        encoded.insert(encoded.begin(), first);
+
+        // Iterate through the 'encoded' vector and apply the XOR operation to decode it.
+        for (int i = 1; i < encoded.size(); i++) {
+            encoded[i] = encoded[i - 1] ^ encoded[i];
+        }
+
+        // Return the decoded vector.
         return encoded;
     }
 };
